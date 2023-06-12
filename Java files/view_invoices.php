2d45f9242cbeede08a3155a632bd98c1c50537c5
thermal-headroom <132761994+thermal-headroom@users.noreply.github.com>
2023-06-12 18:11:07 -0500

    2

--- Java files/view_invoices.php
+++ Java files/view_invoices.php
@@ -4,10 +4,17 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 // Get the POST data
-$client_name = $_POST['client_name'];
+$client_name = trim($_POST['client_name']);
 $invoice_number = $_POST['invoice_number'];
 
-// Perform validation here...
+// Perform validation
+if (empty($client_name)) {
+    die('Client name is required.');
+}
+
+if (!is_numeric($invoice_number) || $invoice_number <= 0) {
+    die('Invoice number must be a positive number.');
+}
 
 // Save the data to a JSON file
 $data = [
@@ -16,13 +23,24 @@ $data = [
   // add other fields here
 ];
 
-// Format the file name with the date and invoice number
-$json_file_name = 'invoices/' . date('Y-m-d') . '-' . $invoice_number . '.json';
-file_put_contents($json_file_name, json_encode($data));
+// Format the directory and file names with the date and invoice number
+$invoice_dir = 'invoices/' . date('Y/m');
+if (!file_exists($invoice_dir) && !mkdir($invoice_dir, 0755, true)) {
+    die('Failed to create invoice directory.');
+}
+
+$json_file_name = $invoice_dir . '/' . date('d') . '-' . $invoice_number . '.json';
+if (file_put_contents($json_file_name, json_encode($data)) === false) {
+    die('Failed to write to JSON file.');
+}
 
 // Load the Excel template
 $templatePath = 'path_to_your_template.xlsx';
-$spreadsheet = IOFactory::load($templatePath);
+try {
+    $spreadsheet = IOFactory::load($templatePath);
+} catch (\PhpOffice\PhpSpreadsheet\Reader\Exception $e) {
+    die('Failed to load Excel template: ' . $e->getMessage());
+}
 
 // Fill in the data
 $worksheet = $spreadsheet->getActiveSheet();
@@ -30,9 +48,13 @@ $worksheet->getCell('A1')->setValue($client_name);
 $worksheet->getCell('B1')->setValue($invoice_number);
 
 // Save your filled-in template as a new file
-$excel_file_name = 'invoices/' . date('Y-m-d') . '-' . $invoice_number . '.xlsx';
+$excel_file_name = $invoice_dir . '/' . date('d') . '-' . $invoice_number . '.xlsx';
 $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-$writer->save($excel_file_name);
+try {
+    $writer->save($excel_file_name);
+} catch (\PhpOffice\PhpSpreadsheet\Writer\Exception $e) {
+    die('Failed to write to Excel file: ' . $e->getMessage());
+}
 
 // Redirect to the invoices page
 header('Location: view_invoices.php');
